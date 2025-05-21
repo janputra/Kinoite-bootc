d0a346d66a7d88f24646a0cd643b2a88a67a9029
Jan Sembiring <jan.s@tetradyne.co.kr>
2025-05-21 09:06:51 +0900

    add git-cola to image

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+
+
+
+
 # this installs a package from fedora repos
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 install -y screen virt-manager samba fcitx5 fcitx5-hangul code
 dnf5 install -y steam mangohud gamemode 
-dnf5 install -y plasma-workspace-x11 btop neovim fastfetch
+dnf5 install -y plasma-workspace-x11 btop neovim fastfetch git-cola
 dnf5 install -y tuned-utils tuned-gtk tuned-profiles-atomic tuned-profiles-cpu-partitioning 
 
 # Use a COPR Example:
