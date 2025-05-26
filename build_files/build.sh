ef811c764fb462e55468a8909cd1a6555b4c1c06
Jan Sembiring <jan.s@tetradyne.co.kr>
2025-05-26 22:47:02 +0900

    add gnome-boxes

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf5 install -y screen virt-manager samba fcitx5 fcitx5-hangul code
+dnf5 install -y screen virt-manager gnome-boxes samba fcitx5 fcitx5-hangul code
 dnf5 install -y steam mangohud gamemode 
 dnf5 install -y plasma-workspace-x11 btop neovim fastfetch git-cola
 dnf5 install -y tuned-utils tuned-gtk tuned-profiles-atomic tuned-profiles-cpu-partitioning 
